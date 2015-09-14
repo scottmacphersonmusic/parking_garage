cadf7ff248fa413989b6ac1a41deb8f46337c79a
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-13 22:27:29 -0700

    Skeleton of ParkingGarage object

---
+++ parking_garage.rb
@@ -0,0 +1,16 @@
+class ParkingGarage
+  def initialize
+    @levels = generate_levels
+    @register = Register.new
+  end
+
+  def availability
+    # Iterates through @levels, calls each of their #availability methods and
+    # prints a breakdown by level and summary out to the console.
+
+    # Level 1 - Handicapped: 3, Compact: 27, Standard: 32
+    # Level 2 ...
+    # Total Monthly - ...
+    # Total Hourly - ...
+  end
+end
